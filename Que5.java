dcf6385466accaea38d3ef24ca113a55f548b719
Himanshi Motiram Dalal <himanshidalal21@gmail.com>
2026-03-14 10:00:00 +0530

    Day 5 - Que5.java

---
+++ Que5.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Employee {
+    int empno;
+    String ename;
+    float basic, hra, da;
+    float netpay;
+
+    // Method to calculate net pay
+    float Calculate() {
+        return basic + hra + da;
+    }
+
+    // Method to accept data
+    void havedata() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee Number: ");
+        empno = sc.nextInt();
+        sc.nextLine(); // clear buffer
+
+        System.out.print("Enter Employee Name: ");
+        ename = sc.nextLine();
+
+        System.out.print("Enter Basic Salary: ");
+        basic = sc.nextFloat();
+
+        System.out.print("Enter HRA: ");
+        hra = sc.nextFloat();
+
+        System.out.print("Enter DA: ");
+        da = sc.nextFloat();
+
+        netpay = Calculate(); // calling calculate method
+    }
+
+    // Method to display data
+    void dispdata() {
+        System.out.println("\n--- Employee Details ---");
+        System.out.println("Employee No: " + empno);
+        System.out.println("Employee Name: " + ename);
+        System.out.println("Basic: " + basic);
+        System.out.println("HRA: " + hra);
+        System.out.println("DA: " + da);
+        System.out.println("Net Pay: " + netpay);
+    }
+}
+
+// Main class
+public class Que5 {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+
+        e.havedata();
+        e.dispdata();
+    }
+}
